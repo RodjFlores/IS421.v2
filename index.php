ce411ca9fb3892c8e8f4ae0b25cd7e6421e0a1be
RodjFlores <gunslingerkills@gmail.com>
2017-10-11 19:38:51 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gunsl
+ * Date: 10/4/2017
+ * Time: 6:35 PM
+ */
+echo 'test';
+echo '<br>';
+echo 'THIS SUCKS!!!!! OH NO';
+for($i = 1; $i <= 20; $i++){
+    echo $i + 4;
+    echo '<br>';
+}
+?>
+
